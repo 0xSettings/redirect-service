190f6e03249e9255047785df8133dbff913169eb
0xsettings <bookdjspindim@gmail.com>
2026-02-12 18:40:46 +0100

    ! shorten url

--- src/repository/url_repository.rs
+++ src/repository/url_repository.rs
@@ -3,13 +3,14 @@ use crate::model::short_key::ShortKey;
 use crate::model::errors::DomainError;
 
 /// Returns `Ok(Some(url))` if found, `Ok(None)` if not found, `EDomain err` on DB error.
-pub trait UrlRepository {
+#[async_trait::async_trait]
+pub trait UrlRepository: Send + Sync {
     
-    pub fn find_by_original_url(&self, original_url: &str) -> Result<Option<Url>, DomainError>;
+    async fn find_by_original_url(&self, original_url: &str) -> Result<Option<Url>, DomainError>;
     
-    pub fn find_by_short_key(&self, short_key: &ShortKey) -> Result<Option<Url>, DomainError>;
+    async fn find_by_short_key(&self, short_key: &ShortKey) -> Result<Option<Url>, DomainError>;
 
-    pub fn insert(&self, url: &Url) -> Result<(), DomainError>;
+    async fn insert(&self, url: &Url) -> Result<(), DomainError>;
 }
 
 
