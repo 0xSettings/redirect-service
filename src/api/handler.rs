978ce766e3646945f84fdc85eb100bb7bba08b3b
0xsettings <bookdjspindim@gmail.com>
2026-02-15 01:11:20 +0100

    ! shorten url

--- src/api/handler.rs
+++ src/api/handler.rs
@@ -1,45 +1,54 @@
 use std::sync::Arc;
 use serde::Deserialize;
-use crate::service::url_service::UrlService; // ← add this line
-
-use axum::{Json, extract::{Path, State}, http::StatusCode, response::{IntoResponse, Redirect}};
-use crate::model::{DomainError, ShortKey, UrlResponse};
+use crate::service::url_service::UrlService;
+use crate::model::{DomainError, ShortKey, UrlResponse, Url};
+use axum::{
+    Json,
+    extract::{Path, State},
+    http::StatusCode,
+    response::{IntoResponse, Redirect},
+};
 
 pub type AppState = Arc<UrlService>;
 
-
-
 #[derive(Deserialize)]
 pub struct ShortenReq {
-    pub url:String,
+    pub url: String,
 }
 
-pub async fn shorten_url (State(service): State<AppState>, 
-Json(body): Json<ShortenReq>) -> impl IntoResponse {
-    match service.shorten(&body.url).await {
+pub async fn shorten_url(
+    State(service): State<AppState>,
+    Json(body): Json<ShortenReq>,
+) -> impl IntoResponse {
+    let result: Result<Url, DomainError> = service.shorten(&body.url).await;
+    match result {
         Ok(url) => {
             let response = UrlResponse::new(url, "http://localhost:8080");
             (StatusCode::CREATED, Json(response)).into_response()
         }
         Err(e) => {
-            eprintln!("Error: {e}"); 
+            eprintln!("Error: {e}");
             (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()).into_response()
         }
     }
 }
 
-pub async fn redirect_to_url(State(service): State<AppState>, 
-Path(key): Path<String>,) -> impl IntoResponse {
+pub async fn redirect_to_url(
+    State(service): State<AppState>,
+    Path(key): Path<String>,
+) -> impl IntoResponse {
     let short_key = match ShortKey::new(key) {
         Ok(k) => k,
         Err(_) => return StatusCode::BAD_REQUEST.into_response(),
     };
 
-    match service.resolve(&short_key).await {
+    let result: Result<Url, DomainError> = service.resolve(&short_key).await;
+    match result {
         Ok(url) => Redirect::permanent(&url.original_url).into_response(),
         Err(DomainError::UrlNotFound) => StatusCode::NOT_FOUND.into_response(),
         Err(e) => {
-            eprintln!("Error: {e}"); StatusCode::INTERNAL_SERVER_ERROR.into_response()
+            eprintln!("Error: {e}");
+            StatusCode::INTERNAL_SERVER_ERROR.into_response()
         }
     }
 }
